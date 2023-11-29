b9fd9a68f5943d3f7e07590d6dae4b1e42838aa5
Parisa <parni1990@gmail.com>
2023-11-29 13:55:47 +0330

    templates
    create statics
    create base.html
    placement templates
    create urls of template

--- Website/settings.py
+++ Website/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'web',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ web/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,JsonResponse
+
+def index(request):
+    return render(request,'site/index.html')
+
+
+def about(request):
+    return render(request,'site/about.html')
+
+
+def contact(request):
+    return render(request,'site/contact.html')
+
